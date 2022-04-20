ce3f6d8dee17ba5ae2ed9d9cd6135e1d731ce3e5
brown52230 <brown52230@mvctc>
2022-04-20 10:16:15 -0400

    defined vehicle base class

---
+++ Vehicle.cpp
@@ -0,0 +1,11 @@
+#include "Vehicle.h"
+
+void Vehicle::setManufacturer(string n)
+{
+    manufacturer = n;
+}
+
+void Vehicle::setYear(int y)
+{
+    year = y;
+}
---
+++ Vehicle.h
@@ -0,0 +1,38 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle
+{
+    private:
+        string manufacturer;
+        int year;
+    public:
+        Vehicle()
+        {
+            // Manufacturer will display NaN if it fails.
+            manufacturer = "NaN";
+            // Year will display 0 if it fails.
+            year = 0;
+        }
+
+        void displayInfo()
+        {
+            cout << "Vehicle Information:" << endl
+                 << "\tManufacturer: " << manufacturer << endl
+                 << "\tYear Built: " << year << endl;
+        }
+
+        void setManufacturer(string);
+        void setYear(int);
+
+        string getManufacturer()
+        { return manufacturer; }
+        int getYear()
+        { return year; }
+};
+
+#endif
